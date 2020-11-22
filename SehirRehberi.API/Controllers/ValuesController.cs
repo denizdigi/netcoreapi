13ed97076375be6293fd5cceff17958df7e1e7f8
webnetdigi <webnetdigi@gmail.com>
2020-11-22 10:04:02 +0300

    first commit

---
+++ SehirRehberi.API/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SehirRehberi.API.Data;
+
+namespace SehirRehberi.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/values
+        //[HttpGet]
+        //public ActionResult<IEnumerable<string>> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        [HttpGet]
+        public ActionResult GetValues() //bunuda kullanabiliriz
+        {
+            var values = _context.Values.ToList();
+            return Ok(values); //action result ile ok dönülebilir ve ok ile değerleride döndük (response içinde değerler)
+        }
+
+        // GET api/values/5
+        //[HttpGet("{id}")]
+        //public ActionResult<string> Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetValue(int id) //async kullanımı task ve await unutulmamalı
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
